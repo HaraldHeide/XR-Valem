01895faabc6e2576002f191e54cad938d022037c
Harald Heide Gundersen <harald.heide.gundersen@hotmail.com>
2020-04-20 16:47:04 +0200

    Before Hand animation

--- Assets/_hhe/XRInteractionToolkit/Scripts/HandPresence.cs
+++ Assets/_hhe/XRInteractionToolkit/Scripts/HandPresence.cs
@@ -5,25 +5,65 @@ using UnityEngine.XR;
 
 public class HandPresence : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public bool showController = false;
+    public InputDeviceCharacteristics controllerCharacteristics;
+    public List<GameObject> controllerPrefabs;
+    public GameObject handModelPrefab;
+
+    private InputDevice targetDevice;
+    private GameObject spawnedController;
+    private GameObject spawnedHandModel;
+
     void Start()
     {
         List<InputDevice> devices = new List<InputDevice>();
-        //InputDevices.GetDevices(devices);
 
-        InputDeviceCharacteristics rightControllerCharacteristics = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
-        InputDevices.GetDevicesWithCharacteristics(rightControllerCharacteristics, devices);
+        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
 
         foreach (var item in devices)
         {
             Debug.Log(item.name + " " + item.characteristics);
         }
 
+        if (devices.Count > 0)
+        {
+            targetDevice = devices[0];
+            GameObject prefab = controllerPrefabs.Find(controller => controller.name == targetDevice.name);
+            if (prefab)
+            {
+                spawnedController = Instantiate(prefab, transform);
+            }
+            else
+            {
+                Debug.LogError("Did not find corresponding controller model");
+                spawnedController = Instantiate(controllerPrefabs[0], transform);
+            }
+
+            spawnedHandModel = Instantiate(handModelPrefab, transform);
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(showController)
+        {
+            spawnedHandModel.SetActive(false);
+            spawnedController.SetActive(true);
+        }
+        else
+        {
+            spawnedHandModel.SetActive(true);
+            spawnedController.SetActive(false);
+        }
+
+
+        //if (targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue) && primaryButtonValue)
+        //    Debug.Log("Pressing Primary Button");
+
+        //if (targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue) && triggerValue > 0.1f)
+        //    Debug.Log("Trigger pressed: " + triggerValue);
+
+        //if (targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 primary2DAxisValue) && primary2DAxisValue != Vector2.zero)
+        //    Debug.Log("Primary Touchpad: " + primary2DAxisValue);
     }
 }
